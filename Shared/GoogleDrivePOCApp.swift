69882a259cfcc2ac5cf60690ea0637b370ddbc2e
Karla <mariakarla.pangilinan@quipper.com>
2022-07-17 17:46:17 +0800

    Create initial project

---
+++ Shared/GoogleDrivePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleDrivePOCApp.swift
+//  Shared
+//
+//  Created by Karla Pangilinan on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GoogleDrivePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
