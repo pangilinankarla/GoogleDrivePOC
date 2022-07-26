17710e73198d712ce76cd279d01f64182c8c7722
Karla <mariakarla.pangilinan@quipper.com>
2022-07-26 23:12:32 +0800

    Update Google URL Types generator

--- google-sign-in.sh
+++ google-sign-in.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Constants
-REVERSE_GOOGLE_CLIENT_ID="com.googleusercontent.apps.1033453333453-ssjdi116776oaj7mtu0dqi9jbpnt7m9l"
+REVERSE_GOOGLE_CLIENT_ID="com.googleusercontent.apps.68133388709-7usqlsr4638s5qlu72irfo2i1oti81pj"
 INFO_PLIST="GoogleDrivePOC-Info.plist"
 KEY_URL_TYPE="CFBundleURLTypes"
 KEY_URL_SCHEME="CFBundleURLSchemes"
@@ -11,7 +11,7 @@ echo "Setting URL Scheme for Google Sign In..."
 checking=true
 i=0
 while [[ $checking == true ]] ; do
-  /usr/libexec/PlistBuddy -c "print :$KEY_URL_TYPE:$i" "$INFO_PLIST" 2>/dev/null
+  /usr/libexec/PlistBuddy -c "print :$KEY_URL_TYPE:$i" $INFO_PLIST 2>/dev/null
   if [ $? -ne 0 ]; then
     checking=false
     break
@@ -23,9 +23,8 @@ checking=true
 clientIdFound=false
 j=$(($i - 1))
 while [[ $j -gt -1 && $checking == true ]] ; do
-  clientId=$(/usr/libexec/PlistBuddy -c "print :$KEY_URL_TYPE:$j:$KEY_URL_SCHEME:0" "${INFO_PLIST}" 2>/dev/null)
-  exitCode1=$?
-  if [[ "$REVERSE_GOOGLE_CLIENT_ID" == "$clientId" ]]; then
+  clientId=$(/usr/libexec/PlistBuddy -c "print :$KEY_URL_TYPE:$j:$KEY_URL_SCHEME:0" $INFO_PLIST 2>/dev/null)
+  if [[ $REVERSE_GOOGLE_CLIENT_ID == $clientId ]]; then
     checking=false
     clientIdFound=true
     break
@@ -35,10 +34,10 @@ done
 
 # Create client id if it doesn't exist
 if [[ $clientIdFound == true ]]; then
-  echo "Google client ID already exists"
+  echo "Google client ID already exists."
 else
-  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE array" "${INFO_PLIST}";
-  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE:$i:$KEY_URL_SCHEME array" "${INFO_PLIST}";
-  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE:$i:$KEY_URL_SCHEME:0 string ${REVERSE_GOOGLE_CLIENT_ID}" "${INFO_PLIST}";
+  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE array" $INFO_PLIST;
+  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE:$i:$KEY_URL_SCHEME array" $INFO_PLIST;
+  /usr/libexec/PlistBuddy -c "add :$KEY_URL_TYPE:$i:$KEY_URL_SCHEME:0 string $REVERSE_GOOGLE_CLIENT_ID" $INFO_PLIST;
   echo "Google client ID added."
 fi
