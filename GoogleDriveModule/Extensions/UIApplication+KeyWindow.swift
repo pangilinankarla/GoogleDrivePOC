71c00fc2f9d420cccf726d07ca5f4e0cef700558
Karla <mariakarla.pangilinan@quipper.com>
2022-07-27 10:37:52 +0800

    Add UIApplication extension

---
+++ GoogleDriveModule/Extensions/UIApplication+KeyWindow.swift
@@ -0,0 +1,23 @@
+//
+//  UIApplication+KeyWindow.swift
+//  GoogleDrivePOC
+//
+//  Created by Karla Pangilinan on 7/27/22.
+//
+
+import SwiftUI
+
+extension UIApplication {
+  var keyWindow: UIWindow? {
+    // Get connected scenes
+    return UIApplication.shared.connectedScenes
+      // Keep only active scenes, onscreen and visible to the user
+      .filter { $0.activationState == .foregroundActive }
+      // Keep only the first `UIWindowScene`
+      .first(where: { $0 is UIWindowScene })
+      // Get its associated windows
+      .flatMap({ $0 as? UIWindowScene })?.windows
+      // Keep only the key window
+      .first(where: \.isKeyWindow)
+  }
+}
