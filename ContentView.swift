59d89219cfff37defafc65ab247a38d90182be74
Karla <mariakarla.pangilinan@quipper.com>
2022-07-27 11:10:59 +0800

    Update views and navigation to SignInView

--- ContentView.swift
+++ ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  // TODO: [learn iOS] (6) environment obj
+  @EnvironmentObject var authViewModel: GoogleAuthenticationViewModel
+
+  var body: some View {
+    // TODO: [learn iOS] (7) emit signout & disconnect
+    Text("Hello, \(authViewModel.viewState.user?.profile?.givenName ?? "world")!")
+      .padding()
+    if authViewModel.viewState.user == nil {
+      SignInView()
+        .environmentObject(authViewModel)
+    } else {
+      Button("SIGN OUT") {
+        authViewModel.emits(.signOut)
+      }.padding()
+
+      Button("DISCONNECT") {
+        authViewModel.emits(.disconnect)
+      }.padding()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- GoogleDrivePOCApp.swift
+++ GoogleDrivePOCApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct GoogleDrivePOCApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  // TODO: [learn iOS] (1) Add GoogleAuthenticationViewModel
+  private var googleAuthViewModel: GoogleAuthenticationViewModel!
+
+  init() {
+    // TODO: [learn iOS] (2) init
+    googleAuthViewModel = GoogleViewModelProvider.shared.provideGoogleAuthenticationViewModel()
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        NavigationLink {
+          ContentView()
+            // TODO: [learn iOS] (5) environment obj
+            .environmentObject(googleAuthViewModel)
+        } label: {
+          Text("Continue to Content View")
         }
+      }
+      .onOpenURL { url in
+        // TODO: [learn iOS] (3) open URL
+        googleAuthViewModel.handleUrl(url)
+      }
+      .onAppear {
+        // TODO: [learn iOS] (4) on appear
+        googleAuthViewModel.emits(.restorePreviousSignIn)
+      }
     }
+  }
 }
