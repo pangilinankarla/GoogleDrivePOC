6e4b30b83720b510270d6ce178a70976df9a4f97
Karla <pangilinan.karla@gmail.com>
2022-08-22 11:03:10 +0800

    Add PhotoPicker

---
+++ Picker/PhotoPicker.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoPicker.swift
+//  GoogleDrivePOC
+//
+//  Created by Karla Pangilinan on 8/18/22.
+//
+
+import SwiftUI
+
+struct PhotoPicker: UIViewControllerRepresentable {
+  @Binding var imageUrl: NSURL
+  
+  func makeUIViewController(context: Context) -> UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    // Not implemented
+  }
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(photoPicker: self)
+  }
+
+  final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    private let photoPicker: PhotoPicker
+
+    init(photoPicker: PhotoPicker) {
+      self.photoPicker = photoPicker
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      // TODO: Replace with PHPicker
+      let imageUrl = info[.imageURL] as? NSURL
+      let imageName = imageUrl?.lastPathComponent
+      print("IMAGE URL : \(String(describing: imageUrl))")
+      print("IMAGE NAME : \(String(describing: imageName))")
+      
+      if imageUrl != nil {
+        photoPicker.imageUrl = imageUrl!
+      }
+      
+      picker.dismiss(animated: true)
+    }
+  }
+}
